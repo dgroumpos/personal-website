3fd8265f87a1a4ed5022785f01bc4b43a2b81a60
dgroumpos <dngroumpos@hotmail.com>
2024-11-23 10:01:58 +0200

    updated project icons, cleaned up projects file

---
+++ src/common/data/projects.js
@@ -0,0 +1,33 @@
+import shoppersList from '../../assets/shoppers-list.png';
+import weatherSage from '../../assets/weathersage.png';
+import betSeer from '../../assets/betseer.png';
+import friendlyPaws from '../../assets/friendlypaws.png';
+
+const projects = [
+  {
+    title: 'Shoppers list',
+    thumbnail: shoppersList,
+    url: 'https://github.com/dgroumpos/Shoppers-List',
+    description: 'Coming soon!',
+  },
+  {
+    title: 'Weather Sage',
+    thumbnail: weatherSage,
+    url: 'https://github.com/dgroumpos/Weather-Sage',
+    description: 'Coming soon!',
+  },
+  {
+    title: 'Bet Seer',
+    thumbnail: betSeer,
+    url: 'https://github.com/dgroumpos/Bet-Seer',
+    description: 'Coming soon!',
+  },
+  {
+    title: 'Friendly Paws',
+    thumbnail: friendlyPaws,
+    url: 'https://github.com/dgroumpos/Friendly-Paws',
+    description: 'Coming soon!',
+  },
+];
+
+export default projects;
--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,39 +1,24 @@
 import styles from './ProjectStyles.module.css';
-import viberr from '../../assets/viberr.png';
-import freshBurger from '../../assets/fresh-burger.png';
-import hipsster from '../../assets/hipsster.png';
-import fitLift from '../../assets/fitlift.png';
 import ProjectCard from '../../common/ProjectCard';
+import projects from '../../common/data/projects';
 
 function Projects() {
   return (
     <section id='projects' className={styles.container}>
       <h1 className='sectionTitle'>Projects</h1>
       <div className={styles.projectsContainer}>
-        <ProjectCard
-          src={viberr}
-          link={'https://github.com/dgroumpos/Reactivities'}
-          h3={'Sample project 1'}
-          p={'Sample project 1'}
-        />
-        <ProjectCard
-          src={freshBurger}
-          link={'https://github.com/dgroumpos/Reactivities'}
-          h3={'Sample project 2'}
-          p={'Sample project 2'}
-        />
-        <ProjectCard
-          src={hipsster}
-          link={'https://github.com/dgroumpos/Reactivities'}
-          h3={'Sample project 3'}
-          p={'Sample project 3'}
-        />
-        <ProjectCard
-          src={fitLift}
-          link={'https://github.com/dgroumpos/Reactivities'}
-          h3={'Sample project 4'}
-          p={'Sample project 4'}
-        />
+        {projects.map((project) => {
+          let { title, thumbnail, url, description } = project;
+          return (
+            <ProjectCard
+              key={url}
+              h3={title}
+              src={thumbnail}
+              link={url}
+              p={description}
+            />
+          );
+        })}
       </div>
     </section>
   );
